c9a7f8ef12fa3c5754a91e17b3f4551c84faf249
Bhasker Goel <bhasker2000goel@gmail.com>
2020-03-26 18:06:57 +0530

    Added Requests table in admin panel

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,5 +1,18 @@
 from django.views.generic import ListView
 from . import models
+from django.http import JsonResponse
+
+
+def approve_request(request):
+    try:
+        pk = request.GET.get('pk')
+        req = models.Request.objects.get(pk=pk)
+    except models.Request.DoesNotExist as e:
+        return JsonResponse({'error': e})
+
+    req.status_completed = True
+    req.save()
+    return JsonResponse({'approved': 'True'})
 
 
 class RequestList(ListView):
