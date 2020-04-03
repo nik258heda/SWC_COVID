210e5bc5a02d3e0b7df1144260c5178ecb3674fc
Pranav <pranavgupta000@gmail.com>
2020-04-03 12:29:54 +0530

    hosting changes

--- SWC_COVID/settings.py
+++ SWC_COVID/settings.py
@@ -100,9 +100,9 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'gis',
         'USER': 'user001',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'PASSWORD': 'Koq35689',
+        'HOST': 'swc-1565.postgres.pythonanywhere-services.com',
+        'PORT': '11565'
     }
 }
 
